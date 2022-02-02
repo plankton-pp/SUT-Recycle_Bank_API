3bbbf4c4422f2e856f0d399b3f9876025c23b194
Thepnimitthip <71626602+Thepnimitthip@users.noreply.github.com>
2022-02-02 19:03:51 +0700

    update SQL Statement

--- services/report2.service.js
+++ services/report2.service.js
@@ -4,12 +4,22 @@ const conn = require('../config/dbConfig')
 
 getMemberByRole = (role) => {
     return new Promise(async (resolve, reject) => {
-        try {
-            const sql = "SELECT ID, Firstname, Lastname, Role, Bank, Acc_number, Phone_number, Phone_number2, Email, Remark FROM members WHERE Role like ?";
-            const result = await conn.query(sql, [role]);
-            resolve(result);
-        } catch (e) {
-            reject(e);
+        if(role != ""){
+            try {
+                const sql = "SELECT ID, Firstname, Lastname, Role, Bank, Acc_number, Phone_number, Phone_number2, Email, Remark FROM members WHERE Role like ?";
+                const result = await conn.query(sql, [role]);
+                resolve(result);
+            } catch (e) {
+                reject(e);
+            }
+        }else{
+            try {
+                const sql = "SELECT ID, Firstname, Lastname, Role, Bank, Acc_number, Phone_number, Phone_number2, Email, Remark FROM members";
+                const result = await conn.query(sql);
+                resolve(result);
+            } catch (e) {
+                reject(e);
+            }
         }
     });
 };
