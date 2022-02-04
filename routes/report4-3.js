211da29abd7c3a21fd57af8886abeae1d79eec58
Thepnimitthip <71626602+Thepnimitthip@users.noreply.github.com>
2022-02-04 09:36:06 +0700

    report 4-3 Summary of deposits and withdrawals

---
+++ routes/report4-3.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const services = require('../services/report4-3.service')
+
+//retrieve Summary of deposits and withdrawals
+router.get("/", async (req, res) => {
+    try {
+        const result = await services.getSumOfDepoAndWithd();
+        let message = ""
+        if (result === undefined || result.length == 0) {
+            message = "Summary of deposits and withdrawals is empty";
+        } else {
+            message = "Successfully retrieved all Summary";
+        }
+        return res.send({ error: false, data: result, message: message })
+    } catch (e) {
+        throw e;
+    }
+});
+
+
+module.exports = router;
+
+
+
