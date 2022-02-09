12dca045ace4fe237ffc9c18b858259c21f39416
Sleeper <Wuttipong.Khumpon@gmail.com>
2022-02-10 01:50:43 +0700

    update table product add Fee_ID

--- routes/product.js
+++ routes/product.js
@@ -10,7 +10,7 @@ router.get("/", async (req, res) => {
         if (result === undefined || result.length == 0) {
             message = "Product table is empty";
         } else {
-            message = "Successfully retrieved all books";
+            message = "Successfully retrieved all Products";
         }
         return res.send({ error: false, data: result, message: message })
     } catch (e) {
@@ -43,17 +43,18 @@ router.post("/", async (req, res) => {
         let detail = req.body.detail;        
         let price = req.body.price; 
         let unitdetail = req.body.unitdetail;    
-        let fee = req.body.fee;           
+        let feeid = req.body.feeid;           
         let createby = req.body.createby;
         
-        // console.log("result",req);
-        const results = await services.addProduct(typeid, name, detail, price, unitdetail, fee, createby);
+        price = parseFloat(price).toFixed(2);
+        
+        const results = await services.addProduct(typeid, name, detail, price, unitdetail, feeid, createby);
         
         //validation
-        if (!typeid || !name|| !price|| !fee || !createby || !unitdetail) {
-            return res.status(400).send({ error: true, message: 'Please provide product\'s typeid name price fee unitdetail and createby.' })
+        if (!typeid || !name|| !price|| !feeid || !createby || !unitdetail) {
+            return res.status(400).send({ error: true, message: 'Please provide product\'s typeid name price feeid unitdetail and createby.' })
         } else {
-            return res.send({ error: false, data: results, message: 'type successfully added' })
+            return res.send({ error: false, data: results, message: 'Product successfully added' })
         }
     } catch (e) {
         throw e;
@@ -68,7 +69,7 @@ router.delete("/:id", async (req, res) => {
         const results = await services.deleteProductById(id);
         //validation
         if (!id) {
-            return res.status(400).send({ error: true, message: 'Please provide product\'s id.' })
+            return res.status(400).send({ error: true, message: 'Please provide Product\'s id.' })
         } else {
             let message = ""
             if (results.affectedRows === 0) {
@@ -93,13 +94,16 @@ router.put("/", async (req, res) => {
         let detail = req.body.detail;        
         let price = req.body.price;            
         let unitdetail = req.body.unitdetail;
+        let feeid = req.body.feeid;
         let updateby = req.body.updateby;     
         let productid = req.body.productid;  
         
-        const results = await services.updateProductById(typeid, name, detail, price, unitdetail, updateby, productid);
+        price = parseFloat(price).toFixed(2);
+        
+        const results = await services.updateProductById(typeid, name, detail, price, unitdetail, feeid, updateby, productid);
         //validation
-        if (!productid|| !typeid|| !name|| !price|| !updateby|| !unitdetail) {
-            return res.status(400).send({ error: true, message: 'Please provide pruduct\'s productid typeid name price updateby unitdetail.' })
+        if (!productid|| !typeid|| !name|| !price|| !unitdetail || !feeid || !updateby) {
+            return res.status(400).send({ error: true, message: 'Please provide pruduct\'s productid typeid name price unitdetail fee updateby .' })
         } else {
             let message = ""
             if (results.changedRows === 0) {
@@ -115,28 +119,28 @@ router.put("/", async (req, res) => {
 });
 
 //update Fee
-router.put("/fee", async (req, res) => {
-    try {
-        //console.log(req);
-        let fee = req.body.fee;  
+// router.put("/fee", async (req, res) => {
+//     try {
+//         //console.log(req);
+//         let fee = req.body.fee;  
         
-        const results = await services.updateFee(fee);
-        //validation
-        if (!fee) {
-            return res.status(400).send({ error: true, message: 'Please provide fee.' })
-        } else {
-            let message = ""
-            if (results.changedRows === 0) {
-                message = "Product not found ";
-            } else {
-                message = "fee successfully updated";
-            }
-            return res.send({ error: false, data: results, message: message })
-        }
-    } catch (e) {
-        throw e;
-    }
-});
+//         const results = await services.updateFee(fee);
+//         //validation
+//         if (!fee) {
+//             return res.status(400).send({ error: true, message: 'Please provide fee.' })
+//         } else {
+//             let message = ""
+//             if (results.changedRows === 0) {
+//                 message = "Product not found ";
+//             } else {
+//                 message = "fee successfully updated";
+//             }
+//             return res.send({ error: false, data: results, message: message })
+//         }
+//     } catch (e) {
+//         throw e;
+//     }
+// });
 
 
 module.exports = router;
--- services/product.service.js
+++ services/product.service.js
@@ -24,13 +24,37 @@ getProducts = () => {
 //     });
 // };
 
-addProduct = (typeid, name, detail, price, unitdetail, fee, createby) => {
+addProduct = (typeid, name, detail, price, unitdetail, feeid
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    , createby) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const sql = "INSERT INTO `product` (`Product_ID`, `Type_ID`, `Name`, `Detail`,`Price_per_unit`, `Unit_Detail`,`Operation_Fee`, `Update_Date`, `Update_By`, `Create_Date`, `Create_By`) VALUES (NULL, ?, ?, ?, ?, ?, ?, '', '', UNIX_TIMESTAMP(NOW()), ?);";
+            const sql = "INSERT INTO `product` (`Product_ID`, `Type_ID`, `Name`, `Detail`, `Price_per_unit`, `Unit_Detail`, `Fee_ID`, `Update_Date`, `Update_By`, `Create_Date`, `Create_By`) VALUES (NULL, ?, ?, ?, ?, ?, ?, '', '', UNIX_TIMESTAMP(NOW()), ?);";
             // console.log("SQL",sql);
             // console.log("typeid, name, detail, price, createby",typeid, name, detail, price, createby);
-            const result = await conn.query(sql, [typeid, name, detail, price, unitdetail, fee, createby]);
+            const result = await conn.query(sql, [typeid, name, detail, price, unitdetail, feeid, createby]);
             
             resolve(result);
         } catch (e) {
@@ -51,11 +75,11 @@ deleteProductById = (id) => {
     });
 };
 
-updateProductById = (typeid, name, detail, price, unitdetail, updateby, productid) => {
+updateProductById = (typeid, name, detail, price, unitdetail, feeid, updateby, productid) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const sql = "UPDATE Product SET Type_ID = ?, Name = ?, Detail = ?, Price_per_unit = ?, Unit_Detail = ?, Update_By = ?, Update_Date = UNIX_TIMESTAMP(NOW()) WHERE Product_ID = ?";
-            const result = await conn.query(sql, [typeid, name, detail, price, unitdetail, updateby, productid]);        
+            const sql = "UPDATE Product SET Type_ID = ?, Name = ?, Detail = ?, Price_per_unit = ?, Unit_Detail = ?, Fee_ID = ?, Update_By = ?, Update_Date = UNIX_TIMESTAMP(NOW()) WHERE Product_ID = ?";
+            const result = await conn.query(sql, [typeid, name, detail, price, unitdetail, feeid, updateby, productid]);        
             // console.log("typeid, name, detail, price, createby",typeid, name, detail, price, createby);
             resolve(result);
         } catch (e) {
