dcfc8fe5cc6759f431bc751080d017b089f4ccef
Sleeper <Wuttipong.Khumpon@gmail.com>
2022-02-11 04:00:00 +0700

    update select column

--- services/report4-2.service.js
+++ services/report4-2.service.js
@@ -15,7 +15,7 @@ getUser = () => {
 getUserplacedetail = (placeid) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const sql = `SELECT * FROM (SELECT Product_ID,Name FROM product) 
+            const sql = `SELECT * FROM (SELECT Product_ID,Name,Price_per_unit as price FROM product) 
             e LEft JOIN (SELECT Product_ID,sum(Unit) FROM placedetail ,place WHERE placedetail.Place_ID = place.Place_ID AND place.Place_ID = ? GROUP BY Product_ID) f
             ON e.Product_ID = f.Product_ID`;
             const result = await conn.query(sql, [placeid]);
