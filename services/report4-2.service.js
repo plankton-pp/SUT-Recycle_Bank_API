0b52f1029749ecdf9938e88b1d3bfc1f6df1b938
Sleeper <Wuttipong.Khumpon@gmail.com>
2022-02-12 03:46:42 +0700

    edit code in deposit and update sql 4-2

--- services/report4-2.service.js
+++ services/report4-2.service.js
@@ -12,13 +12,13 @@ getUser = () => {
     });
 };
 
-getUserplacedetail = (placeid) => {
+getUserplacedetail = (memid) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const sql = `SELECT * FROM (SELECT Product_ID,Name,Price_per_unit as price FROM product) 
-            e LEft JOIN (SELECT Product_ID,sum(Unit) FROM placedetail ,place WHERE placedetail.Place_ID = place.Place_ID AND place.Place_ID = ? GROUP BY Product_ID) f
-            ON e.Product_ID = f.Product_ID`;
-            const result = await conn.query(sql, [placeid]);
+            const sql = `SELECT * FROM (SELECT Product_ID as PID,Name,Price_per_unit as price FROM product) e
+            LEft JOIN (SELECT Product_ID,sum(Unit) FROM placedetail ,place WHERE placedetail.Place_ID = place.Place_ID AND place.Member_ID = ? GROUP BY Product_ID) f
+            ON e.PID = f.Product_ID`;
+            const result = await conn.query(sql, [memid]);
             resolve(result);
         } catch (e) {
             reject(e);
