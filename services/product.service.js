89b37d1330ca9b820d4ff328f6096dad64874bc9
Thepnimitthip <71626602+Thepnimitthip@users.noreply.github.com>
2022-02-21 16:28:30 +0700

    fix: addproduct and addtype to insert Update_By

--- services/product.service.js
+++ services/product.service.js
@@ -44,10 +44,10 @@ getProducts = () => {
 addProduct = (typeid, name, detail, price, unitdetail, createby) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const sql = "INSERT INTO `product` (`Product_ID`, `Type_ID`, `Name`, `Detail`, `Price_per_unit`, `Unit_Detail`, `Update_Date`, `Update_By`, `Create_Date`, `Create_By`) VALUES (NULL, ?, ?, ?, ?, ?, '', '', UNIX_TIMESTAMP(NOW()), ?);";
+            const sql = "INSERT INTO `product` (`Product_ID`, `Type_ID`, `Name`, `Detail`, `Price_per_unit`, `Unit_Detail`, `Update_Date`, `Update_By`, `Create_Date`, `Create_By`) VALUES (NULL, ?, ?, ?, ?, ?, UNIX_TIMESTAMP(NOW()), ?, UNIX_TIMESTAMP(NOW()), ?);";
             // console.log("SQL",sql);
             console.log("typeid, name, detail, price, createby",typeid, name, detail, price, createby);
-            const result = await conn.query(sql, [typeid, name, detail, price, unitdetail, createby]);
+            const result = await conn.query(sql, [typeid, name, detail, price, unitdetail, createby, createby]);
             
             resolve(result);
         } catch (e) {
