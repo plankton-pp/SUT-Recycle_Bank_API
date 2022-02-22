9a793ec2f72162810fed0a93df7e7671204c3524
CuttO-Pxhrl <planktonplnt@gmail.com>
2022-02-22 08:12:51 +0700

    fix: add emp

--- services/employee.service.js
+++ services/employee.service.js
@@ -124,9 +124,9 @@ resetPassword = (email, newpassword) => {
 addNewEmployee = (Empid, Email) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const sql = `INSERT INTO employee (Firstname, Lastname, Username, Password, Role, Phone, Employee_ID, Email) VALUES( ?, ?, ?, ?, ?, ?, ?, ?)`;
-            const result = await conn.query(sql, ["-", "-", "-", "-", "-", "-", Empid, Email]);
-            resolve(result);
+            const sql = `INSERT INTO employee (Firstname, Lastname, Username, Password, Role, Phone, Employee_ID, Email) VALUES( "-","-","-","-","-","-",${Empid},${Email})`;
+            // const result = await conn.query(sql, ["-", "-", "-", "-", "-", "-", Empid, Email]);
+            resolve({ message: sql });
         } catch (e) {
             reject(e);
         }
